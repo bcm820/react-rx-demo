bb2b676188462b8f776a49f9aa43ec0f14e40844
Brian Mendoza <brian.mendoza@deciphernow.com>
2018-05-25 00:31:50 -0400

    update docs

--- src/rxstore/index.js
+++ src/rxstore/index.js
@@ -1,13 +1,27 @@
 import { BehaviorSubject } from 'rxjs';
-import { defaultIfEmpty } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { observe } from './Observer';
 import data from '../data';
 
-// initialState will be the initial state of our observable
-// for this app, the initial state is one of three preset spinner configurations
+// `initialState` is passed into our BehaviorSubject constructor
+// as the initial state of our application's shared state.
+// In this app, it is one of three preset spinner configurations.
 const initialValue = data.basic;
 
-// rxStore$ is what stores and emits each new iteration of state
-// via an asynchronous event stream, multicast to all its subscribers
+// `rxStore$` stores and emits each new iteration of `rxState`
+// via an asynchronous event stream, multicast to all its subscribers.
 const rxStore$ = new BehaviorSubject(initialValue);
-export const rxConnect = observe(rxStore$, defaultIfEmpty(initialValue));
+
+// For convenience, we can export a partially applied function
+// that receives a component to wrap and pass rxState into.
+export const rxConnect = observe(rxStore$);
+
+// Rxjs uses many "pipeable" operators, pure functions that
+// can be added as additional arguments to our `observe` function
+// to modify the behavior of the rxStore$ subscription.
+const mirrorAnimation = map(next => ({
+  ...next,
+  positionX: next.positionX === 'left' ? 'right' : 'left',
+  positionY: next.positionY === 'bottom' ? 'top' : 'bottom'
+}));
+export const rxConnectMirror = observe(rxStore$, mirrorAnimation);
